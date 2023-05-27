fdb9a617fa0669c0bd84dbd311251c011e8fb2c1
Chandler Newman <chandler2newman@hotmail.co.uk>
2023-05-27 19:55:25 +0100

    Add on-disk kv db

--- cmd/mediaserver/main.go
+++ cmd/mediaserver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/csnewman/cathode/mediaserver"
 	"golang.org/x/exp/slog"
@@ -11,7 +12,25 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 		Level:     slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if len(groups) == 0 && a.Key == slog.SourceKey {
+				//nolint:forcetypeassert
+				source := a.Value.Any().(*slog.Source)
+				source.File = strings.TrimPrefix(source.File, "github.com/csnewman/cathode/")
+			}
+
+			return a
+		},
 	}))
 
-	mediaserver.NewServer(logger)
+	logger.Info("Cathode MediaServer")
+
+	ms := mediaserver.NewServer(logger)
+	defer ms.Close()
+
+	if err := ms.Run(); err != nil {
+		logger.Error("Server Crashed", "err", err)
+	} else {
+		logger.Error("Server Stopped")
+	}
 }
--- mediaserver/mediaserver.go
+++ mediaserver/mediaserver.go
@@ -1,13 +1,59 @@
 package mediaserver
 
-import "golang.org/x/exp/slog"
+import (
+	"log"
+
+	"github.com/csnewman/cathode/shared"
+	"golang.org/x/exp/slog"
+)
 
 type Server struct {
 	logger *slog.Logger
+	db     *shared.DB
 }
 
 func NewServer(logger *slog.Logger) *Server {
+	db, err := shared.NewDB(logger, "db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return &Server{
 		logger: logger,
+		db:     db,
 	}
+}
+
+func (s *Server) Close() error {
+	return s.db.Close()
+}
+
+type Test struct {
+	Server string `json:"server"`
+	Made   bool   `json:"made"`
+}
+
+func (s *Server) Run() error {
+	return s.db.Transact(true, func(tx *shared.Tx) error {
+		var test Test
+
+		if err := tx.Get("test", &test); err != nil {
+			s.logger.Error("test", "e", err)
+
+			return err
+		}
+
+		test = Test{
+			Server: "123",
+			Made:   true,
+		}
+
+		if err := tx.Set("test", test); err != nil {
+			s.logger.Error("test-set", "e", err)
+
+			return err
+		}
+
+		return nil
+	})
 }
